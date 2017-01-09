92bc6c7ec38fb174e780d164509519d58764fa2f
Atsuko <oda-atsuko@av.avex.co.jp>
2017-01-09 15:21:44 +0000

    05_実習_ユーザ新規作成画面

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :thumbnail, UserThumbnailUploader
+  validates :name, presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
 end
