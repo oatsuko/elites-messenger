8c8668175d3e6fff4d9d0c20e4f40f5c0c10d7a4
Atsuko <oda-atsuko@av.avex.co.jp>
2017-01-10 04:29:49 +0000

    11_実習_投稿ユーザのフィルタリング

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,7 +3,8 @@ class TimelinesController < ApplicationController
     # メッセージ入力
     @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     # タイムラインを取得
-    @timeline = Timeline.includes(:user).order('updated_at DESC')
+    @timeline = Timeline.includes(:user).user_filter(params[:filter_user_id]).order('updated_at DESC')
+    @users = User.all
   end
   
   def create
@@ -27,6 +28,15 @@ class TimelinesController < ApplicationController
       flash[:alert] = timeline.errors.full_messages
     end
     redirect_to action: :index
+  end
+  
+  def filter_by_user
+    if params[:filter_user_id].present?
+      redirect_to action: :index, filter_user_id: params[:filter_user_id]
+    else
+      # フィルターなし
+      redirect_to action: :index
+    end
   end
 
   private
